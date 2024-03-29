ee696f0ff564694f823c7cb9d8af6eca13db5122
efmorales <54375116+efmorales@users.noreply.github.com>
2024-03-29 16:34:41 -0400

    added HeroCard component and helpers to display hero data

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../helpers'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
-    
-  return (
-    <>
-        <h1>{publisher}</h1>
-        <hr />
-        <ul>
-            {
-                heroes.map(hero => (
-                    <li key={hero.id}>{hero.superhero}</li>
-                ))
-            }
-        </ul>
-    
-    </>
-  )
+
+    return (
+        <>
+            <h1>{publisher}</h1>
+            <hr />
+            <div
+                className='row row-cols-1 row-cols-md-3 g-3'
+            >
+                {
+                    heroes.map(hero => (
+                        <HeroCard
+                            key={hero.id}
+                            {...hero}
+                        />
+                    ))
+                }
+            </div>
+
+        </>
+    )
 }
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,17 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
+
 export const HeroPage = () => {
+
+  const { id } = useParams();
+
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
+
   return (
     <h1>HeroPage</h1>
   )
